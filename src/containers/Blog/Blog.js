ad100dffee3228110d2dda51c74139e230735dc4
Hisni <hisnionline@gmail.com>
2019-04-02 23:39:19 +0530

    route test

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,16 +1,17 @@
 import React, { Component } from 'react';
 import { Route } from 'react-router-dom';
 //import classes from './Blog.css';
-import Posts from './Posts/Posts';
 import Layout from '../../hoc/Layout/Layout';
-
+import Posts from './Posts/Posts';
+import NewPosts from './NewPost/NewPost';
 
 class Blog extends Component {
     render () {
         return (
             <div>
                 <Layout>
-                    <Route path="/" exact render={ () => <h1>Home</h1>} />
+                    <Route path="/" exact component={Posts} />
+                    <Route path="/post-add" component={NewPosts} />
                 </Layout>
             </div>
         );
