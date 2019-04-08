028a9d2bff4064f61c38cac120940432e467093a
Hisni <hisnionline@gmail.com>
2019-04-08 19:40:04 +0530

    Nav bar update

--- Source/src/components/Navigation/NavigationItems/NavigationItems.js
+++ Source/src/components/Navigation/NavigationItems/NavigationItems.js
@@ -6,7 +6,7 @@ import NavigationItem from './NavigationItem/NavigationItem';
 const navigationItems = () => (
     <ul className={classes.NavigationItems}>
         <NavigationItem link="/posts">Posts</NavigationItem>
-        <NavigationItem link="/post-add">NewAD</NavigationItem>
+        <NavigationItem link="/post-add">New Post</NavigationItem>
         <NavigationItem link="/login">Login</NavigationItem>
         <NavigationItem link="/register">Register</NavigationItem>
     </ul>
