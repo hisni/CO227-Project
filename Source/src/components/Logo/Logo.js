dab9fc199799e5cb8c1ce8a0bf063d8aeb54b799
Hisni <hisnionline@gmail.com>
2019-04-17 21:19:33 +0530

    Styling Edits

--- Source/src/components/Logo/Logo.js
+++ Source/src/components/Logo/Logo.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
-import Logo from '../../assets/images/Logo.jpg';
+import Logo from '../../assets/images/Logo.png';
 import classes from './Logo.css';
 
 const logo = (props) => (
