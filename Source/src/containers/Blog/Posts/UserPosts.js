0dc79d224ddc036ba1a034950b0c8ce865d4a974
Hisni <hisnionline@gmail.com>
2019-04-19 22:46:48 +0530

    User Specific Post Added

--- Source/src/containers/Blog/Posts/UserPosts.js
+++ Source/src/containers/Blog/Posts/UserPosts.js
@@ -1,17 +1,25 @@
 import React, { Component } from 'react';
 import axios from 'axios';
-//import {Link} from 'react-router-dom';
+import { connect } from 'react-redux';
+
 import Post from '../../../components/Tile/Post/Post';
 import Spinner from '../../../components/UI/Spinner/Spinner';
 import classes from './Posts.css';
 
 class Posts extends Component {
     state = {
-        posts: null
+        posts: null,
     }
 
     componentDidMount () {
-        axios.get( '/Posts.json' )
+        
+        var queryParams = null;
+
+        if( this.props.isAuthenticated ){
+            queryParams = '/Posts.json?orderBy="UID"&equalTo="' + this.props.UID + '"';
+        }
+
+        axios.get( queryParams )
             .then( response => {
                 const fetchedPosts = [];
                 for(let key in response.data){
@@ -26,12 +34,7 @@ class Posts extends Component {
     }
 
     postSelectedHandler = (id) => {
-        console.log(this.props.match.params.district);
-        if( this.props.match.params.district ){
-            this.props.history.push({pathname: '/posts/' + this.props.match.params.district + '/' + id});
-        }else{
-            this.props.history.push({pathname: '/posts/all/' + id});
-        }
+        this.props.history.push({pathname: '/posts/user/' + id});
     }
 
     render() {
@@ -61,4 +64,11 @@ class Posts extends Component {
     };
 }
 
-export default Posts;
+const mapStateToProps = state => {
+    return {
+        isAuthenticated: state.auth.token !== null,
+        UID: state.auth.userId
+    }
+}
+
+export default connect(mapStateToProps)(Posts);
