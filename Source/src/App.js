d139045325d9e393b38a974fbc2dfd3cf6c38970
Hisni <hisnionline@gmail.com>
2019-04-27 12:56:08 +0530

    Code Structure Improved

--- Source/src/App.js
+++ Source/src/App.js
@@ -1,16 +1,77 @@
 import React, { Component } from 'react';
+import { Route, Switch, Redirect, withRouter } from 'react-router-dom';
+import { connect } from 'react-redux';
+
 import classes from './App.css';
-import Blog from './containers/Blog/Blog';
+import Layout from './hoc/Layout/Layout';
+import Posts from './containers/Blog/Posts/Posts';
+import NewPost from './containers/Blog/NewPost/NewPost';
+import FullPost from './containers/Blog/FullPost/FullPost';
+import Login from './containers/Auth/Login';
+import Signup from './containers/Auth/Signup';
+import HomePage from './containers/HomePage/HomePage';
+import Logout from './containers/Auth/Logout';
+import UserPosts from './containers/Blog/Posts/UserPosts';
+import * as actions from './store/actions/index';
 
 class App extends Component {
 
-  render() {
-    return (
-        <div className={classes.App}>
-          <Blog/>
-        </div>
-    );
-  }
+    componentDidMount(){
+        this.props.onTryAutoSignup();
+    }
+
+    render() {
+
+        let routes = (
+            <Switch>
+                <Route path="/" exact component={HomePage} />
+                <Route path="/login" exact component={Login} />                        
+                <Route path="/register" exact component={Signup} />                                                
+                <Route path="/posts/all" exact component={Posts} />                        
+                <Route path="/posts/:district" exact component={Posts} />                        
+                <Route path="/posts/all/:id" exact component={FullPost} />                                                
+                <Route path="/posts/:district/:id" exact component={FullPost} />
+                <Redirect to="/" />
+            </Switch>
+        );
+      
+        if ( this.props.isAuthenticated ) {
+            routes = (
+                <Switch>
+                    <Route path="/" exact component={HomePage} />
+                    <Route path="/post-add" exact component={NewPost} />
+                    <Route path="/logout" exact component={Logout} />
+                    <Route path="/posts/all" exact component={Posts} />                        
+                    <Route path="/posts/user" exact component={UserPosts} />                    
+                    <Route path="/posts/:district" exact component={Posts} />                        
+                    <Route path="/posts/user/:id" exact component={FullPost} />                        
+                    <Route path="/posts/all/:id" exact component={FullPost} />                                                
+                    <Route path="/posts/:district/:id" exact component={FullPost} />   
+                    <Redirect to="/" />
+                </Switch>
+            );
+        }
+
+        return (
+            <div className={classes.App}>
+                <Layout>
+                    {routes}
+                </Layout>
+            </div>
+        );
+    }
 }
 
-export default App;
+const mapStateToProps = state => {
+    return {
+        isAuthenticated: state.auth.token !== null
+    };
+};
+
+const mapDispatchToProps = dispatch => {
+    return {
+        onTryAutoSignup: () => dispatch( actions.authCheckState() )
+    };
+};
+
+export default withRouter( connect( mapStateToProps, mapDispatchToProps )( App ));
